5cf96b940925146786f6755c9897cb9a47dd3ff7
qqqbbb <qqqinbox@ya.ru>
2022-08-06 16:06:42 +0300

    Initial commit

---
+++ src/main/java/qqqbbb/homework2dot3/SpringHomeworkApplication.java
@@ -0,0 +1,15 @@
+package qqqbbb.springHomework2dot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHomeworkApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringHomeworkApplication.class, args);
+    }
+
+}
