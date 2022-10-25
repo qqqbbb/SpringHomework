47a0971d97e65a7a3a3c3b736a6dc68fa7084f3d
qqqbbb <qqqinbox@ya.ru>
2022-10-25 10:07:54 +0300

    Homework 3.2 fix1

--- src/main/java/qqqbbb/hogwarts/school/Main.java
+++ src/main/java/qqqbbb/hogwarts/school/Main.java
@@ -9,20 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @OpenAPIDefinition
 public class Main
 {
-//    static HashMap<Integer, Student> m = new HashMap<Integer, Student>();
     public static void main(String[] args)
     {
         SpringApplication.run(Main.class, args);
-//        m.put(1, new Student(1L, "name!", 11));
-//        Student student = m.get(1);
-//        System.out.println(student.getName());
-//        m.remove(1);
-//        System.out.println("size " + m.size());
-//        System.out.println(student.getName());
-//        StudentController studentController = new StudentController(new StudentService());
-//        StudentService studentService = new StudentService();
-//        List<Student> students = studentService.getAllStudents().stream().filter(s -> s.getAge() == 1).collect(Collectors.toList());
-//        System.out.println(students.size());;
+
     }
 }
 
